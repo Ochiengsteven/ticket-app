1172b03f2370ffa1fba4c16b87b824fbe4dbf90f
Ochiengsteven <stevenochieng6305@gmail.com>
2024-04-25 16:50:50 +0300

    UPDATE VIEWS TO USE DB DATA

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,10 +1,28 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { useRouter } from "next/navigation";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+
+    if (res.ok) {
+      router.refresh();
+    } else {
+      console.log("Failed to delete ticket.");
+    }
+  };
+
   return (
     <FontAwesomeIcon
+      onClick={deleteTicket}
       icon={faTrash}
       className="text-red-400 hover:cursor-pointer hover:text-red-200"
     />
--- app/(components)/Status.jsx
+++ app/(components)/Status.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+const Status = ({ status }) => {
+  let statusColorClass;
 
-const Status = () => {
+  switch (status) {
+    case "Open":
+      statusColorClass = "bg-green-400";
+      break;
+    case "In Progress":
+      statusColorClass = "bg-blue-400";
+      break;
+    case "Closed":
+      statusColorClass = "bg-gray-400";
+      break;
+    default:
+      statusColorClass = "bg-gray-400";
+  }
+
   return (
     <div className="mt-2">
-      <span className="text-xs text-gray-700 font-semibold py-1 px-2 inline-block rounded-full bg-green-400">
-        Open
+      <span
+        className={`text-xs text-gray-700 font-semibold py-1 px-2 inline-block rounded-full ${statusColorClass}`}
+      >
+        {status}
       </span>
     </div>
   );
